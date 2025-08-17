0eb9fe38413332e6f9c762f73147059f688856d6
YordanYordanov <yordanyordanov0104@gmail.com>
2025-08-17 23:01:51 +0300

    Update author-show.blade.php

--- resources/views/livewire/pages/author-show.blade.php
+++ resources/views/livewire/pages/author-show.blade.php
@@ -3,17 +3,10 @@
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div class="md:col-span-1">
-             @if (!empty($author['photo']))
-                @php
-                    $photo = $author['photo'];
-
-                    if (!Str::startsWith($photo, ['http://', 'https://'])) {
-                        $photo = asset('storage/' . ltrim($photo, '/'));
-                    }
-                @endphp
-
-                <img src="{{ $photo }}" alt="Снимка на {{ $author['name'] }}"
-                    class="w-full h-auto rounded-2xl shadow-md mb-4" itemprop="image">
+            @if (!empty($author['photo']))
+                <img src="{{ asset('storage/authors/' . ltrim($author['photo'], '/')) }}"
+                    alt="Снимка на {{ $author['name'] }}" class="w-full h-auto rounded-2xl shadow-md mb-4"
+                    itemprop="image">
             @endif
 
 

0407a470804ecd0cd99434b013e608f5a7b29599
YordanYordanov <yordanyordanov0104@gmail.com>
2025-10-31 23:51:17 +0200

    Update pagination.blade.php

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,24 +1,24 @@
 @if ($paginator->hasPages())
     @php
-        $pages = collect($paginator->toArray()['links'])->filter(fn($el) => is_numeric($el['label']));
+        $pages = collect($paginator->toArray()['links'])
+            ->filter(fn($el) => is_numeric($el['label']));
     @endphp
 
-    <nav x-data class="flex items-center justify-center gap-2 mt-8 select-none">
-
+    <nav x-data class="flex items-center justify-center gap-2 mt-10">
         @foreach ($pages as $el)
             @if ($el['active'])
-                <span
-                    class="min-w-[36px] h-9 flex items-center justify-center rounded-lg border bg-black text-white font-semibold">
+                <span class="min-w-[42px] h-11 flex items-center justify-center rounded-xl border border-black bg-black text-white font-semibold text-base">
                     {{ $el['label'] }}
                 </span>
             @else
-                <button wire:click="gotoPage({{ $el['label'] }})"
-                    @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
-                    class="min-w-[36px] h-9 flex items-center justify-center rounded-lg border hover:bg-black hover:text-white transition">
+                <button
+                    wire:click="gotoPage({{ $el['label'] }})"
+                    @click="window.scrollTo({ top: document.querySelector('#catalog-title').offsetTop - 20, behavior: 'smooth' })"
+                    class="min-w-[42px] h-11 flex items-center justify-center rounded-xl border border-gray-400 hover:bg-black hover:text-white text-base transition"
+                >
                     {{ $el['label'] }}
                 </button>
             @endif
         @endforeach
-
     </nav>
 @endif

5247663c68f499d63668cb1c1addf1334697fcfb
YordanYordanov <yordanyordanov0104@gmail.com>
2025-11-09 11:16:10 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email === 'yordan@gmail.com';
+        return $this->email === 'izdatelstvosatori@gmail.com';
     }
 
     /** @use HasFactory<\Database\Factories\UserFactory> */

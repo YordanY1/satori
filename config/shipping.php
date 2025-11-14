9c5569d875ab7ff24015710466b26b53be00aa53
YordanYordanov <yordanyordanov0104@gmail.com>
2025-11-14 23:18:56 +0200

    Update shipping.php

--- config/shipping.php
+++ config/shipping.php
@@ -3,11 +3,11 @@
 // config/shipping.php
 return [
     'econt' => [
-        'sender_name'  => 'Йордан Атанасов Йорданов',
-        'sender_phone' => '0899030455',
-        'sender_city'  => 'Асеновград',
-        'sender_post'  => '4230',
-        'sender_street' => 'ул. Места',
-        'sender_num'   => '1',
+        'sender_name'  => 'СаториКо',
+        'sender_phone' => '0878490782',
+        'sender_city'  => 'София',
+        'sender_post'  => '1632',
+        'sender_street' => 'ж.к. Овча Купел 1, бл. 411',
+        'sender_num'   => '2',
     ],
 ];
